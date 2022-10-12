3b47736c0c4ebfebb12f64b42623cb0a21697c81
Sureshkumar T <tsureshkumar2000@gmail.com>
2022-10-12 17:04:41 +0530

    few updates

--- dots/dot-vim-ycm_extra_conf.py
+++ dots/dot-vim-ycm_extra_conf.py
@@ -7,11 +7,13 @@ import ycm_core
 # compilation database set (by default, one is not set).
 # CHANGE THIS LIST OF FLAGS. YES, THIS IS THE DROID YOU HAVE BEEN LOOKING FOR.
 flags = [
+        'dasa',
     '-Wall',
     '-Wextra',
     '-Werror',
     '-Wno-long-long',
     '-Wno-variadic-macros',
+    '-Wc++11-extensions',
     '-fexceptions',
     # THIS IS IMPORTANT! Without a "-std=<something>" flag, clang won't know which
     # language to use when compiling headers. So it will guess. Badly. So C++
@@ -19,7 +21,7 @@ flags = [
     # a "-std=<something>".
     # For a C project, you would set this to something like 'c99' instead of
     # 'c++11'.
-    '-std=c++14',
+    '-std=c++20',
     # ...and the same thing goes for the magic -x option which specifies the
     # language that the files to be compiled are written in. This is mostly
     # relevant for c++ headers.
